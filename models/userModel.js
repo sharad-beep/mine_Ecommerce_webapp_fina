fd58a666cf825ff82e3f842697f6400a5a05be3b
sharad-beep <70638080+sharad-beep@users.noreply.github.com>
2023-05-14 15:24:06 +0530

    Ecommerce_code
    ecommerce_mern_stack_app

---
+++ models/userModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: false,
+    },
+    answer: {
+      type: {},
+      required: false,
+    },
+
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("users", userSchema);
